dec6f7874be606a1b53e7e3af4dcfadb5dc32363
atul sharma <atulsharma@atuls-MacBook-Pro.local>
2025-02-15 20:59:52 +0530

    update file

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,8 @@
 Calculator functions
 """
 
-def add(x,y):
-    """Add x and y and return result,"""
-    return x+y
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,15 +1,17 @@
 """
 Sample tests
 """
+
 from django.test import SimpleTestCase
 
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """Tests the calc module"""
 
     def test_add_number(self):
         """Adding test cal numbers"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
